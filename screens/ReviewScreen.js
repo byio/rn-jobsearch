adbefd4e2139ea681ae32d4aa4057233c8546166
byio <ben@benyang.io>
2018-04-06 22:33:19 +0800

    add headerStyle object to navigationOptions to apply platform specific styles for the android platform

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
 import { Button } from 'react-native-elements';
 
 class ReviewScreen extends Component {
   // define class level properties to specify route-related configurations
   static navigationOptions = ({ navigation }) => ({
     title: 'Review Jobs',
-    headerRight: <Button title='Settings' onPress={() => { navigation.navigate('settingsRoute') }} />
+    headerRight: (
+      <Button
+        backgroundColor='rgba(0, 0, 0, 0)'
+        color='rgba(0, 122, 255, 1)'
+        title='Settings'
+        onPress={() => { navigation.navigate('settingsRoute') }}
+      />
+    ),
+    headerStyle: {
+      marginTop: Platform.OS === 'android' ? 24 : 0
+    }
   });
 
   // render method
