8c424133298bef911639f85dc9bc2aed0723b71c
byio <ben@benyang.io>
2018-04-06 22:24:33 +0800

    use class level properties to configure navigation options for ReviewScreen, and create a button that routes to the SettingsScreen with navigation.navigate()

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 
 class ReviewScreen extends Component {
+  // define class level properties to specify route-related configurations
+  static navigationOptions = ({ navigation }) => ({
+    title: 'Review Jobs',
+    headerRight: <Button title='Settings' onPress={() => { navigation.navigate('settingsRoute') }} />
+  });
+
   // render method
   render () {
     return (
