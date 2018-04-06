c8a102259c77ed90532645c5a9971ae06601fc06
byio <ben@benyang.io>
2018-04-06 15:27:02 +0800

    implement TabNavigator from react-navigation on the WelcomeScreen and AuthScreen screens

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TabNavigator } from 'react-navigation';
+
+// import screens
+import WelcomeScreen from './screens/WelcomeScreen';
+import AuthScreen from './screens/AuthScreen';
 
 export default class App extends React.Component {
+  // render method
   render() {
+    const MainNavigator = TabNavigator({
+      welcomeRoute: { screen: WelcomeScreen },
+      authRoute: { screen: AuthScreen }
+    });
+
     return (
-      <View style={styles.container}>
-        <Text>RN JobSearch App</Text>
-      </View>
+      // <View style={styles.container}>
+      <MainNavigator/>
+      // </View>
     );
   }
 }
