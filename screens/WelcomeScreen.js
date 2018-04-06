a549288b85f6751b754d66e9b55f5242a48b659a
byio <ben@benyang.io>
2018-04-06 22:49:27 +0800

    create array of slides data to be passed as data props into the Slides component from the WelcomeScreen screen

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -3,12 +3,19 @@ import { View, Text } from 'react-native';
 
 import Slides from '../components/Slides';
 
+// data to be rendered on slides
+const SLIDE_DATA = [
+  { text: 'Welcome to Native Jobs!' },
+  { text: 'Use this app to find a job in your native location.' },
+  { text: 'Simply set your location, and swipe away!' }
+];
+
 class WelcomeScreen extends Component {
   // render method
   render () {
     return (
       <View>
-        <Slides/>
+        <Slides data={SLIDE_DATA} />
       </View>
     );
   }
