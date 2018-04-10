3157eee31eda93a9878d2725f7b12b3af82fbb6c
byio <ben@benyang.io>
2018-04-10 22:11:01 +0800

    create a component-level state with a region property in MapScreen which will be used in MapView to render initial location; location set to DeLaveaga Park in Santa Cruz

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -3,12 +3,23 @@ import { View, Text } from 'react-native';
 import { MapView } from 'expo';
 
 class MapScreen extends Component {
+  // component-level state to handle initial region in mapView
+  state = {
+    region: {
+      longitude: -122,
+      latitude: 37,
+      longitudeDelta: 0.04,
+      latitudeDelta: 0.09
+    }
+  }
+
   // render method
   render () {
     return (
       <View style={styles.container}>
         <MapView
           style={styles.mapView}
+          region={this.state.region}
         />
       </View>
     );
