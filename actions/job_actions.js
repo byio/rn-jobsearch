fde76cb796bb0ddeb52b5a55814378e8617cf10b
byio <ben@benyang.io>
2018-04-11 14:25:24 +0800

    generate query url by passing the reverseGeocoded zip into a helper function that makes use of the npm qs package to create a concatenated string with the root url, a collection of key-value pairs (query params), and the zipcode

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -1,12 +1,34 @@
 import axios from 'axios';
 import reverseGeocode from 'latlng-to-zip';
+import qs from 'qs';
+
+// import indeed credentials
+import { INDEED_PUBLISHER_ID } from 'react-native-dotenv';
 
 // import action types
 import { FETCH_JOBS } from './types';
 
+// define indeed url and query parameters
+const QUERY_ROOT_URL = 'http://api.indeed.com/ads/apisearch?';
+const JOB_QUERY_PARAMS = {
+  publisher: INDEED_PUBLISHER_ID,
+  format: 'json',
+  v: '2',
+  latlong: 1,
+  radius: 10,
+  q: 'javascript'
+};
+
+// helper functions
+const buildQueryUrl = (zip) => {
+  const query = qs.stringify({ ...JOB_QUERY_PARAMS, l: zip });
+  return `${QUERY_ROOT_URL}${query}`;
+};
+
 export default fetchJobs = (region) => return async dispatch => {
   try {
     let zip = await reverseGeocode(region);
+    const queryUrl = buildQueryUrl(zip);
   } catch (err) {
     console.log(err);
   }
