1e65abc91fe27649bcf7dbd22ac7d3f1472be17d
byio <ben@benyang.io>
2018-04-11 09:24:34 +0800

    create new action type of FETCH_JOBS, and create new empty job action fetchJobs that will eventually handle fetching of jobs from api

---
+++ actions/job_actions.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+// import action types
+import { FETCH_JOBS } from './types';
+
+export default fetchJobs = (region) => return async dispatch => {
+  
+};
--- actions/types.js
+++ actions/types.js
@@ -1,2 +1,6 @@
+// auth action types
 export const FACEBOOK_LOGIN_SUCCESS = 'facebook_login_success';
 export const FACEBOOK_LOGIN_FAIL = 'facebook_login_fail';
+
+// job action types
+export const FETCH_JOBS = 'fetch_jobs';
