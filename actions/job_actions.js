6aad2006f636033a757686b613ac807801f96216
byio <ben@benyang.io>
2018-04-11 13:50:06 +0800

    convert longitude and latitude data from MapScreen to zipcode in fetchJobs job action creator with the help of npmpackage latlng-to-zip

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
+import reverseGeocode from 'latlng-to-zip';
 
 // import action types
 import { FETCH_JOBS } from './types';
 
 export default fetchJobs = (region) => return async dispatch => {
-  
+  try {
+    let zip = await reverseGeocode(region);
+  } catch (err) {
+    console.log(err);
+  }
 };
