10958c73ff913b313cc71deb5a22300c252bf36a
byio <ben@benyang.io>
2018-04-12 16:01:53 +0800

    import and render Swipe component in DeckScreen, and pass the jobs data as props to the Swipe component

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -2,22 +2,23 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 
+import Swipe from '../components/Swipe';
+
 class DeckScreen extends Component {
   // render method
   render () {
     return (
       <View>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
+        <Swipe
+          data={this.props.jobs}
+        />
       </View>
     );
   }
 }
 
 const mapStateToProps = ({ jobs }) => {
-  return { jobs };
+  return { jobs: jobs.results };
 };
 
 export default connect(mapStateToProps)(DeckScreen);
