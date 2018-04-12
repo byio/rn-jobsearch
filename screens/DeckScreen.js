2cb55e41770ee6561cf4c7e291b4783696b2d255
byio <ben@benyang.io>
2018-04-13 00:19:25 +0800

    render unique MapView in each card with initialRegion its reflecting the lat-long coordinates in each job listed in the returned response

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform, Dimensions } from 'react-native';
 import { connect } from 'react-redux';
 import { Card, Button } from 'react-native-elements';
+import { MapView } from 'expo';
 
 import Swipe from '../components/Swipe';
+
+// define constants
+const SCREEN_HEIGHT = Dimensions.get('window').height;
 
 class DeckScreen extends Component {
   // helper functions
   renderCard = (job) => {
+    const initialRegion = {
+      longitude: job.longitude,
+      latitude: job.latitude,
+      longitudeDelta: 0.045,
+      latitudeDelta: 0.02
+    };
     return (
       <Card title={job.jobtitle}>
+        <View style={styles.mapContainer}>
+          <MapView
+            style={styles.mapView}
+            scrollEnabled={false}
+            cacheEnabled={Platform.OS === 'android' ? true : false}
+            initialRegion={initialRegion}
+          />
+      </View>
         <View style={styles.detailContainer}>
           <Text>{job.company}</Text>
           <Text>{job.formattedRelativeTime}</Text>
@@ -42,6 +60,12 @@ class DeckScreen extends Component {
 }
 
 const styles = {
+  mapContainer: {
+    height: SCREEN_HEIGHT * 0.5
+  },
+  mapView: {
+    flex: 1
+  },
   detailContainer: {
     flexDirection: 'row',
     justifyContent: 'space-around'
