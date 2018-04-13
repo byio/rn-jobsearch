43e30a00641307c8f49f4c2829ded46b2faa3f2b
byio <ben@benyang.io>
2018-04-13 21:56:05 +0800

    export the returned state  from likes reducer as likedJobs via the combineReducers() method in index.js

--- reducers/index.js
+++ reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import auth from './auth_reducer';
 import job from './job_reducer';
+import likedJobs from './likes_reducer';
 
 export default combineReducers({
   auth,
-  job
+  job,
+  likedJobs
 });
