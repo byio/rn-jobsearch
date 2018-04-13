9f3f65fc66779bf3f883a08aabd9238368faad94
byio <ben@benyang.io>
2018-04-13 21:54:21 +0800

    likes reducer will return a unique array of job objects based on the jobkey property, with the help of the lodash module

--- reducers/likes_reducer.js
+++ reducers/likes_reducer.js
@@ -1,11 +1,16 @@
+import _ from 'lodash';
+
 // import action types
 import { LIKE_JOB } from '../actions/types';
 
 // export reducer
-export default (state, action) => {
+export default (state = [], action) => {
   switch (action.type) {
     case LIKE_JOB:
-      
+      // return an array of unique objects based on the jobkey property
+      return _.uniqBy([
+        action.payload, ...state
+      ], 'jobkey');
     default:
       return state;
   }
