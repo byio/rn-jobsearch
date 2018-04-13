810c958ed0312c143e654d2ff0827b4319b97d40
byio <ben@benyang.io>
2018-04-13 21:39:34 +0800

    create boilerplate for likes reducer that will eventually handle the state related to liked jobs

---
+++ reducers/likes_reducer.js
@@ -0,0 +1,12 @@
+// import action types
+import { LIKE_JOB } from '../actions/types';
+
+// export reducer
+export default (state, action) => {
+  switch (action.type) {
+    case LIKE_JOB:
+      
+    default:
+      return state;
+  }
+};
