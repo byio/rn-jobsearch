39d53bdc7278a4ffa4c9b70c0c4954b0be275c8f
byio <ben@benyang.io>
2018-04-16 18:47:40 +0800

    create new action type of clear_liked_jobs

--- actions/types.js
+++ actions/types.js
@@ -5,3 +5,4 @@ export const FACEBOOK_LOGIN_FAIL = 'facebook_login_fail';
 // job action types
 export const FETCH_JOBS = 'fetch_jobs';
 export const LIKE_JOB = 'like_job';
+export const CLEAR_LIKED_JOBS = 'clear_liked_jobs';
