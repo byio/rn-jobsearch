a517a7af7861044f10bf870af6d714168242ed23
byio <ben@benyang.io>
2018-04-20 19:36:54 +0800

    used redux-persist@4 to ensure that likedJobs state persists in AsyncStorage when user leaves app

--- reducers/likes_reducer.js
+++ reducers/likes_reducer.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import { REHYDRATE } from 'redux-persist/constants';
 
 // import action types
 import { LIKE_JOB, CLEAR_LIKED_JOBS } from '../actions/types';
@@ -6,6 +7,8 @@ import { LIKE_JOB, CLEAR_LIKED_JOBS } from '../actions/types';
 // export reducer
 export default (state = [], action) => {
   switch (action.type) {
+    case REHYDRATE:
+      return action.payload.likedJobs || [];
     case LIKE_JOB:
       // return an array of unique objects based on the jobkey property
       return _.uniqBy([
--- store/index.js
+++ store/index.js
@@ -1,12 +1,24 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { AsyncStorage } from 'react-native';
+import { persistStore, autoRehydrate } from 'redux-persist';
 
 import reducers from '../reducers';
 
+// autoRehydrate is a store enhancer that pulls all data out and sending them through all reducers
 const store = createStore(
   reducers,
   {},
-  compose(applyMiddleware(thunk))
+  compose(
+    applyMiddleware(thunk),
+    autoRehydrate()
+  )
+);
+
+// sets up redux-persist to watch for change in state in redux store, and update AsyncStorage accordingly
+persistStore(
+  store,
+  { storage: AsyncStorage, whitelist: ['likedJobs'] }
 );
 
 export default store;
